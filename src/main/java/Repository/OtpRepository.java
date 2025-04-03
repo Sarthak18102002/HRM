e82ef87d129618c2b9a2597d521ce58291ecc502
Sarthak Deshpande <sarthakdeshpande14@gmail.com>
2025-04-03 15:55:16 +0530

    Delete src directory

--- src/main/java/Repository/OtpRepository.java
+++
@@ -1,8 +0,0 @@
-package HRM.FinalProject.Repository;
-import HRM.FinalProject.UserEntity.Otp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OtpRepository extends JpaRepository<Otp, Long> {
-    List<Otp> findByEmail(String email);
-}
--- src/main/java/Repository/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package HRM.FinalProject.Repository;
-
-import HRM.FinalProject.UserEntity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    // Custom queries if needed
-}
